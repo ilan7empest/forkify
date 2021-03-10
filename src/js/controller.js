6ef795ff370ba84ea599689ec154965f9d61260b
ilan7empest <63646693+ilan7empest@users.noreply.github.com>
2021-03-10 10:15:33 +0200

    forkify 1st commit

---
+++ src/js/controller.js
@@ -0,0 +1,155 @@
+// import icons from '../img/icons.svg';
+import icons from 'url:../img/icons.svg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+const BaseURL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+
+const Spinner = parentEl => {
+  const html = `<div class="spinner">
+                  <svg>
+                    <use href="${icons}#icon-loader"></use>
+                  </svg>
+                </div>`;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', html);
+};
+
+const fetchData = async () => {
+  Spinner(recipeContainer);
+  try {
+    const response = await fetch(BaseURL + `/5ed6604591c37cdc054bc886`);
+    let data = await response.json();
+    if (!response.ok) throw new Error(data.message);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      image: recipe.image_url,
+      coockingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceURL: recipe.source_url,
+      title: recipe.title,
+    };
+
+    const markup = `<figure class="recipe__fig">
+                    <img src=${recipe.image} alt="${
+      recipe.title
+    }" class="recipe__img" />
+                    <h1 class="recipe__title">
+                        <span>${recipe.title}</span>
+                    </h1>
+                </figure>
+
+                <div class="recipe__details">
+                    <div class="recipe__info">
+                        <svg class="recipe__info-icon">
+                        <use href="${icons}#icon-clock"></use>
+                        </svg>
+                        <span class="recipe__info-data recipe__info-data--minutes">${
+                          recipe.coockingTime
+                        }</span>
+                        <span class="recipe__info-text">minutes</span>
+                    </div>
+                    <div class="recipe__info">
+                        <svg class="recipe__info-icon">
+                        <use href="${icons}#icon-users"></use>
+                        </svg>
+                        <span class="recipe__info-data recipe__info-data--people">${
+                          recipe.servings
+                        }</span>
+                        <span class="recipe__info-text">servings</span>
+
+                        <div class="recipe__info-buttons">
+                        <button class="btn--tiny btn--increase-servings">
+                            <svg>
+                            <use href="${icons}#icon-minus-circle"></use>
+                            </svg>
+                        </button>
+                        <button class="btn--tiny btn--increase-servings">
+                            <svg>
+                            <use href="${icons}#icon-plus-circle"></use>
+                            </svg>
+                        </button>
+                        </div>
+                    </div>
+
+                    <div class="recipe__user-generated">
+                        <svg>
+                        <use href="${icons}#icon-user"></use>
+                        </svg>
+                    </div>
+                    <button class="btn--round">
+                        <svg class="">
+                        <use href="${icons}#icon-bookmark-fill"></use>
+                        </svg>
+                    </button>
+                </div>
+
+                <div class="recipe__ingredients">
+                    <h2 class="heading--2">Recipe ingredients</h2>
+                    <ul class="recipe__ingredient-list">
+                      ${recipe.ingredients
+                        .map(ingr => {
+                          return `<li class="recipe__ingredient">
+                                  <svg class="recipe__icon">
+                                    <use href="${icons}#icon-check"></use>
+                                  </svg>
+                                  <div class="recipe__quantity">${ingr.quantity}</div>
+                                  <div class="recipe__description">
+                                    <span class="recipe__unit">${ingr.unit}</span>
+                                    ${ingr.description}
+                                  </div>
+                                </li>`;
+                        })
+                        .join('')}
+
+                    </ul>
+                </div>
+
+                <div class="recipe__directions">
+                    <h2 class="heading--2">How to cook it</h2>
+                    <p class="recipe__directions-text">
+                        This recipe was carefully designed and tested by
+                        <span class="recipe__publisher">${
+                          recipe.publisher
+                        }</span>. Please check out
+                        directions at their website.
+                    </p>
+                    <a
+                        class="btn--small recipe__btn"
+                        href=${recipe.sourceURL}
+                        target="_blank"
+                    >
+                        <span>Directions</span>
+                        <svg class="search__icon">
+                        <use href="${icons}#icon-arrow-right"></use>
+                        </svg>
+                    </a>
+                </div>`;
+    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    const message = recipeContainer.querySelector('.message');
+
+    recipe
+      ? (message.style.display = 'none')
+      : (message.style.display = 'block');
+  } catch (err) {
+    console.log('Error', err.message);
+  }
+};
+fetchData();
